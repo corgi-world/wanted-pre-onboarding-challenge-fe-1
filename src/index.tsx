f996992b26f929f712056c6135b96be827f85731
corgi-world <developer.msw@gmail.com>
2022-07-31 16:36:39 +0900

    feat: recoil, router provider 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import GlobalStyles from "./styles/globalStyles";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
